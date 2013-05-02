4ad44a745d5b15410c8787e5dccc4db7a3c960c2
Oliver Caldwell <olliec87@gmail.com>
2013-05-02 22:35:53 +0100

    Events mixin is now upper case and in tests list.

--- tests/main.js
+++ tests/main.js
@@ -1,7 +1,8 @@
 requirejs.config({
 	paths: {
 		chai: '../node_modules/chai/chai',
-		mocha: '../node_modules/mocha/mocha'
+		mocha: '../node_modules/mocha/mocha',
+		tarmac: '../src'
 	},
 	urlArgs: 'v=' + Date.now()
 });
@@ -15,6 +16,10 @@ define([
 	assert = chai.assert;
 
 	require([
+		'specs/utils/blend',
+
+		'specs/mixins/Events',
+
 		'specs/Controller',
 		'specs/Model',
 		'specs/Router',
