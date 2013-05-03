656c9c973fef839169f65348a9f21ab96be4748a
Oliver Caldwell <olliec87@gmail.com>
2013-05-03 22:45:23 +0100

    Corrected a tiny bit of wording in a comment.
    And this is why you go to sleep when you're tired.

--- src/mixin.js
+++ src/mixin.js
@@ -1,7 +1,7 @@
 define(function() {
 	/**
 	 * Mixes all passed objects into the leftmost object. The leftmost objects
-	 * have higher precedence and will overwrite values set by the leftmost
+	 * have higher precedence and will overwrite values set by the rightmost
 	 * objects. This is a shallow merge, it will not recurse into sub-objects.
 	 *
 	 * @param {Object} target Destination object for all other objects values.
