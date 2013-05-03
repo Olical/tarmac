257e6fb6d5557110499921b3d8f32ff8f5eaed96
Oliver Caldwell <olliec87@gmail.com>
2013-05-03 21:51:20 +0100

    Started the Events mixin test structure.

---
+++ tests/specs/mixins/Events.js
@@ -0,0 +1,11 @@
+define([
+	'tarmac/mixins/Events'
+], function(Events) {
+	suite('tarmac/mixins/Events', function() {
+		setup(function() {
+		});
+
+		test('can fetch the events object', function() {
+		});
+	});
+});
