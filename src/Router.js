0fb6e8e46f4bc78b545d1e81b7559c23b7bc27e3
Oliver Caldwell <olliec87@gmail.com>
2013-05-04 18:02:10 +0100

    Added the base router.
    Allows you to add routes. Can't route to anything yet though.

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,72 @@
 define(function() {
+	/**
+	 * The router maps URLs to controllers and defines sections of the URL to
+	 * extract and place inside the request object. It can also be setup to
+	 * pass a context object to all controllers. This object could contain an
+	 * instance of storage for accessing models, for example.
+	 *
+	 * @class
+	 */
 	function Router() {
 	}
+
+	/**
+	 * Creates a route and maps it to a controller with an optional action. If
+	 * you want to use the default controller action mapping provided by the
+	 * base controller class then you must pass an action. The action will map
+	 * to the "{{ action name }}Action" function in the controller.
+	 *
+	 * The route URL can contain segments prefixed with a colon. These will be
+	 * extracted and sent through to the controller in the request object. For
+	 * example: "/user/:id/". This will pass the id through to the controller
+	 * under request.id.
+	 *
+	 * Route segments can contain any alphanumeric character as well as hyphens
+	 * and underscores.
+	 *
+	 * @param {String} route URL to match with segments to extract denoted with a colon.
+	 * @param {Function} controller Controller class to map to, not a class instance, an actual class.
+	 * @param {String} action Optional action string to pass through to the controller.
+	 * @return {Object} The current instance to allow chaining.
+	 */
+	Router.prototype.addRoute = function(route, controller, action) {
+		var routes = this.getRoutes();
+		routes[route] = {
+			route: this._compileRoute(route),
+			controller: controller,
+			action: action
+		};
+
+		return this;
+	};
+
+	/**
+	 * Builds a regular expression from the provided route.
+	 *
+	 * @param {String} route
+	 * @return {RegExp} The built RegExp to match the route.
+	 * @private
+	 */
+	Router.prototype._compileRoute = function(route) {
+		var validRouteSegment = '([a-zA-Z0-9_-]+)';
+		var routeSegmentRegExp = new RegExp(':' + validRouteSegment);
+		var routeRegExpSource = route.replace(routeSegmentRegExp, validRouteSegment);
+		var routeRegExp = new RegExp('^' + routeRegExpSource + '$');
+		return routeRegExp;
+	};
+
+	/**
+	 * Fetches the routes object.
+	 *
+	 * @return {Object}
+	 */
+	Router.prototype.getRoutes = function() {
+		if (typeof this._routes === 'undefined') {
+			this._routes = {};
+		}
+
+		return this._routes;
+	};
 
 	return Router;
 });
--- tests/specs/Router.js
+++ tests/specs/Router.js
@@ -1,6 +1,56 @@
 define([
-	'tarmac/Router'
-], function(Router) {
+	'tarmac/Router',
+	'tarmac/Controller',
+	'sinon'
+], function(Router, Controller) {
+	function SpyController() {
+		this.execute = sinon.spy();
+	}
+	SpyController.prototype = Object.create(Controller.prototype);
+
 	suite('tarmac/Router', function() {
+		setup(function() {
+			this.router = new Router();
+		});
+
+		suite('getRoutes', function() {
+			test('routes starts as an empty object', function() {
+				var routes = this.router.getRoutes();
+				assert.deepEqual(routes, {});
+			});
+		});
+
+		suite('addRoute', function() {
+			test('routes are added to the route object', function() {
+				var route = '/users/:id/';
+				this.router.addRoute(route);
+				var routes = this.router.getRoutes();
+				assert.property(routes, route);
+			});
+
+			test('routes are added with a controller and option', function() {
+				var route = '/users/:id/';
+				this.router.addRoute(route, SpyController, 'testing');
+				var routes = this.router.getRoutes();
+
+				assert.deepPropertyVal(routes, route + '.controller', SpyController);
+				assert.deepPropertyVal(routes, route + '.action', 'testing');
+			});
+
+			test('routes compile a RegExp that matches example routes', function() {
+				var route = '/users/:id/';
+				this.router.addRoute(route);
+				var routes = this.router.getRoutes();
+
+				assert.deepPropertyVal(routes, route + '.route');
+
+				var routeRegExp = routes[route].route;
+
+				assert.isTrue(routeRegExp.test('/users/100/'));
+				assert.isFalse(routeRegExp.test('/users/100/extra/'));
+				assert.isFalse(routeRegExp.test('/users/100'));
+				assert.isFalse(routeRegExp.test('users/100/'));
+			});
+		});
 	});
 });
