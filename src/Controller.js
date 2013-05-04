4b6f7324e045fa7131dee979710b601f7e50b3b0
Oliver Caldwell <olliec87@gmail.com>
2013-05-04 15:48:25 +0100

    Added the base controller and it's execute method.
    The method maps actions to {{ function name }}Action.

--- src/Controller.js
+++ src/Controller.js
@@ -1,6 +1,37 @@
 define(function() {
-	function NAME() {
+	/**
+	 * Controllers are instantiated by the router and have their execute
+	 * method executed when routed to. This class should handle the required
+	 * models and views to render the result. This is pretty much an abstract
+	 * class so your should extend it and add your own functionally.
+	 *
+	 * @class
+	 */
+	function Controller() {
 	}
 
-	return NAME;
+	/**
+	 * Called when the controller has been routed to. Gets passed all of the
+	 * information required to render a response.
+	 *
+	 * By default, the execute method will map the action to a method and pass
+	 * it the request and context objects. The mapping works by appending
+	 * "Action" to the current action name and then calling that method on this
+	 * object.
+	 *
+	 * So calling execute with an action of "createUser" will
+	 * call this.createUserAction.
+	 *
+	 * @param {String|null} action Current action, if any.
+	 * @param {Object} request Contains values extracted from the URL.
+	 * @param {Object} context Object of information that can be passed down from the router.
+	 * @return {Object} The current instance to allow chaining.
+	 */
+	Controller.prototype.execute = function(action, request, context) {
+		var actionHandlerName = action + 'Action';
+		this[actionHandlerName](request, context);
+		return this;
+	};
+
+	return Controller;
 });
--- tests/specs/Controller.js
+++ tests/specs/Controller.js
@@ -1,4 +1,33 @@
-define(function() {
-	suite('NAME', function() {
+define([
+	'tarmac/Controller',
+	'sinon'
+], function(Controller) {
+	suite('tarmac/Controller', function() {
+		setup(function() {
+			this.controller = new Controller();
+		});
+
+		suite('execute', function() {
+			test('executes the correct action', function() {
+				this.controller.spyAction = sinon.spy();
+				this.controller.execute('spy');
+				assert.isTrue(this.controller.spyAction.called);
+			});
+
+			test('passes the request and context to the action', function() {
+				var request = {
+					type: 'request'
+				};
+				var context = {
+					type: 'context'
+				};
+
+				this.controller.spyAction = sinon.spy();
+				this.controller.execute('spy', request, context);
+				var args = this.controller.spyAction.args[0];
+				assert.strictEqual(args[0], request);
+				assert.strictEqual(args[1], context);
+			});
+		});
 	});
 });
