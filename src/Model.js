5b33ea0431124ca972f6a084dc90b3435068e08a
Oliver Caldwell <olliec87@gmail.com>
2013-05-05 11:19:46 +0100

    Added base Model JSDoc and constructor with tests.

--- src/Model.js
+++ src/Model.js
@@ -1,6 +1,19 @@
 define(function() {
-	function NAME() {
+	/**
+	 * This is where you store your data and methods to access it. Keeping
+	 * things simple by giving you a data object to store your data in. No set
+	 * or get methods in the way, they are not required. So if you have an
+	 * array in storage you can just run model.data.someArray.push, no juggling
+	 * .get('someArray').
+	 *
+	 * You can pass an object to the constructor to start it with data.
+	 *
+	 * @param {Object} data Initial data to store in the model.
+	 * @class
+	 */
+	function Model(data) {
+		this.data = data || {};
 	}
 
-	return NAME;
+	return Model;
 });
--- tests/specs/Model.js
+++ tests/specs/Model.js
@@ -1,4 +1,23 @@
-define(function() {
-	suite('NAME', function() {
+define([
+	'tarmac/Model'
+], function(Model) {
+	suite('tarmac/Model', function() {
+		setup(function() {
+			this.model = new Model();
+		});
+
+		suite('Model', function() {
+			test('constructor lets you set data', function() {
+				var data = {
+					foo: true
+				};
+				var model = new Model(data);
+				assert.strictEqual(model.data, data);
+			});
+
+			test('constructor sets a default data object if you do not pass one', function() {
+				assert.isObject(this.model.data);
+			});
+		});
 	});
 });
