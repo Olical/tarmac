a2e4c85f04e69dd5f5af301f12e669e49e140a02
Oliver Caldwell <olliec87@gmail.com>
2013-05-07 21:33:12 +0100

    Added a basic JSDoc to storage.

--- src/Storage.js
+++ src/Storage.js
@@ -1,4 +1,11 @@
 define(function() {
+	/**
+	 * Handles loading and saving your models from a local or remove source.
+	 * Remote could just mean the localStorage object, it does not have to be
+	 * another server or API.
+	 *
+	 * @class
+	 */
 	function Storage() {
 	}
 
