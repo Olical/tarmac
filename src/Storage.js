7e77a1699a7e4de4edb769a7e1025e22efadab84
Oliver Caldwell <olliec87@gmail.com>
2013-05-11 13:59:15 +0100

    Storage sets a default adaptor now.

--- src/Storage.js
+++ src/Storage.js
@@ -1,13 +1,31 @@
-define(function() {
+define([
+	'./storageAdaptors/InstanceStorage'
+], function(InstanceStorage) {
 	/**
 	 * Handles loading and saving your models from a local or remove source.
 	 * Remote could just mean the localStorage object, it does not have to be
 	 * another server or API.
 	 *
+	 * By default, the Storage class will use the instance adaptor which simply
+	 * stores the data within the object. This is not persistent and will be
+	 * lost when the adapter is destroyed.
+	 *
 	 * @class
 	 */
 	function Storage() {
+		this._setStorageAdaptor(new InstanceStorage);
 	}
+
+	/**
+	 * Sets the storage adaptor object. Needs to be an instance/descendent of
+	 * StorageAdaptor.
+	 *
+	 * @param {Object} adaptor
+	 * @private
+	 */
+	Storage.prototype._setStorageAdaptor = function(adaptor) {
+		this._storageAdaptor = adaptor;
+	};
 
 	return Storage;
 });
