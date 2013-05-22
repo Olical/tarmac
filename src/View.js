183c7e7066376875386495c4f558c648365dd30c
Oliver Caldwell <olliec87@gmail.com>
2013-05-22 20:51:15 +0100

    Added a blank render method to View.
    This is so you can see what you have to override and what is expected
    from the API.

--- src/View.js
+++ src/View.js
@@ -9,5 +9,14 @@ define(function() {
 	function View() {
 	}
 
+	/**
+	 * This function should perform the final render of the view. If you are
+	 * working with a DOM it could inject the result into it directly. If you
+	 * are running on a server or something like that you could return the HTML
+	 * and pipe that out to the user.
+	 */
+	View.prototype.render = function() {
+	};
+
 	return View;
 });
